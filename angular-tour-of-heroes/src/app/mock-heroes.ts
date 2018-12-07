4752fa586a69a5668bab38bf0ad1a002d1f7bc80
Jamie <752492509@qq.com>
2018-12-07 17:51:41 +0800

    up

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './hero';
+
+export const HEROES : Hero[] = [
+    { id: 2, name: 'Tom' },
+    { id: 3, name: 'Deer' },
+    { id: 4, name: 'Cat' },
+    { id: 5, name: 'Night' },
+]
