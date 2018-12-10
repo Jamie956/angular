2341db4e63dfe65e07feb0beff353d98319847a0
Jamie <752492509@qq.com>
2018-12-10 16:21:09 +0800

    up

--- angular-tour-of-heroes/src/app/mock-heroes.ts
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -1,8 +1,14 @@
 import { Hero } from './hero';
 
-export const HEROES : Hero[] = [
-    { id: 2, name: 'Tom' },
-    { id: 3, name: 'Deer' },
-    { id: 4, name: 'Cat' },
-    { id: 5, name: 'Night' },
-]
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
