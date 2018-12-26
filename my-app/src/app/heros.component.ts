7f5d8a63579c59308ce5c08d52604bb6453def7f
Jamie <752492509@qq.com>
2018-12-26 11:00:14 +0800

    up

--- my-app/src/app/heros.component.ts
+++ my-app/src/app/heros.component.ts
@@ -308,6 +308,75 @@ export class HeroParentComponent {
   ];
 }
 
+//////////////////////////////
+@Component({
+  selector: "app-name-child",
+  template: '<h3>"{{name}}"</h3>'
+})
+export class NameChildComponent {
+  private _name = "";
+
+  @Input()
+  set name(name: string) {
+    this._name = (name && name.trim()) || "<no name set>";
+  }
+
+  get name(): string {
+    return this._name;
+  }
+}
+
+@Component({
+  selector: "app-name-parent",
+  template: `
+    <app-name-child *ngFor="let name of names" [name]="name"></app-name-child>
+  `
+})
+export class NameParentComponent {
+  names = ["Mr. IQ", "   ", "  Bombasto  "];
+}
+
+//////////////////////////////
+@Component({
+  selector: 'app-voter',
+  template: `
+    <h4>{{name}}</h4>
+    <button (click)="vote(true)"  [disabled]="didVote">Agree</button>
+    <button (click)="vote(false)" [disabled]="didVote">Disagree</button>
+  `
+})
+export class VoterComponent {
+  @Input()  name: string;
+  @Output() voted = new EventEmitter<boolean>();
+  didVote = false;
+
+  vote(agreed: boolean) {
+    this.voted.emit(agreed);
+    this.didVote = true;
+  }
+}
+
+@Component({
+  selector: 'app-vote-taker',
+  template: `
+    <h2>Should mankind colonize the Universe?</h2>
+    <h3>Agree: {{agreed}}, Disagree: {{disagreed}}</h3>
+    <app-voter *ngFor="let voter of voters"
+      [name]="voter"
+      (voted)="onVoted($event)">
+    </app-voter>
+  `
+})
+export class VoteTakerComponent {
+  agreed = 0;
+  disagreed = 0;
+  voters = ['Mr. IQ', 'Ms. Universe', 'Bombasto'];
+
+  onVoted(agreed: boolean) {
+    agreed ? this.agreed++ : this.disagreed++;
+  }
+}
+
 export const heroSwitchComponents = [
   SecondComponent,
   HerosComponent,
@@ -328,5 +397,9 @@ export const heroSwitchComponents = [
   Keyup2Component,
   Keyup3Component,
   HeroChildComponent,
-  HeroParentComponent
+  HeroParentComponent,
+  NameChildComponent,
+  NameParentComponent,
+  VoterComponent,
+  VoteTakerComponent
 ];
